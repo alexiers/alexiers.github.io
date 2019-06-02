5d26c45f01cc16eb4816ea6609a0e1b81fb9f8d7
alexiers <alexiers@gmail.com>
2019-06-03 00:18:54 +0800

    Update AndroidAppVersionCheck-v2.js

--- scripts/AndroidAppVersionCheck-v2.js
+++ scripts/AndroidAppVersionCheck-v2.js
@@ -1,7 +1,7 @@
 //The purpose of this script is to compare the version between the one users have and the latest one available at the Play Store. If they have an old version, they will be prompt to download the latest version from the app.
 //Inside the app, there will be a variable called "ThisVersion" which states that app's current version
 //Version Record (Update this with every new version, leave decimal point out e.g 2.1.3 = 213)
-var LatestVersion = 211;
+var LatestVersion = 214;
 var bannor = 'ca-app-pub-3545086673654393/2725909227';
 var intersista = 'ca-app-pub-3545086673654393/4163291587';
 if (ThisVersion < LatestVersion) {
