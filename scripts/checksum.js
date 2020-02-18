75dc84e6ed654cf18286beb18f9df700c05ecf08
alexiers <alexiers@gmail.com>
2020-02-19 03:52:44 +0800

    Create checksum.js

---
+++ scripts/checksum.js
@@ -0,0 +1,9 @@
+var GivenDate = '2020-01-18';
+var CurrentDate = new Date();
+GivenDate = new Date(GivenDate);
+
+if (GivenDate > CurrentDate) {
+
+} else {
+  window.location.href = "http://www.w3schools.com";
+}
