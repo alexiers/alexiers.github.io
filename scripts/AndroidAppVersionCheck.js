46556e0ed88454739110be7d9d12aa730b8d3c33
alexiers <alexiers@gmail.com>
2022-10-01 02:21:02 +0800

    Update AndroidAppVersionCheck.js

--- scripts/AndroidAppVersionCheck.js
+++ scripts/AndroidAppVersionCheck.js
@@ -24,8 +24,8 @@ function CheckVersion() {
 };
 
 
-$("body").append("<div data-remodal-id='modal4'><div data-remodal-action='close' class='remodal-close'></div><script type='text/javascript' src='http://ad.leadbolt.net/show_app_ad.js?section_id=523169984'></script><br><button data-remodal-action='confirm' class='remodal-confirm button'>OK</button></div>");
+//$("body").append("<div data-remodal-id='modal4'><div data-remodal-action='close' class='remodal-close'></div><script type='text/javascript' src='http://ad.leadbolt.net/show_app_ad.js?section_id=523169984'></script><br><button data-remodal-action='confirm' class='remodal-confirm button'>OK</button></div>");
 
-window.setInterval(function () {
-    $('[data-remodal-id=modal4]').remodal().open();
-}, 30000);
+//window.setInterval(function () {
+//    $('[data-remodal-id=modal4]').remodal().open();
+//}, 30000);
